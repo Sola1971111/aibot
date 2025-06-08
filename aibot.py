b4aef0292fd76c2b44c0aea76fc26e4b35099ad7
Sola1971111 <contact@cooziepicks.com>
2025-06-08 01:31:45 +0100

    new

--- aibot.py
+++ aibot.py
@@ -1413,7 +1413,7 @@ async def send_free_user_reminder(context: ContextTypes.DEFAULT_TYPE, text: str)
         except Exception:
             pass
 
-    tasks = [asyncio.create_task(send(row["user_id"])) for row in users]
+    tasks = [asyncio.create_task(send(row[0])) for row in users]
     if tasks:
         await asyncio.gather(*tasks)
 
@@ -1430,7 +1430,7 @@ async def night_reminder(context: ContextTypes.DEFAULT_TYPE):
     await send_free_user_reminder(context, REMINDER_NIGHT)
 
 
-job_queue.run_daily(morning_reminder, time=time(hour=12, minute=29))
+job_queue.run_daily(morning_reminder, time=time(hour=12, minute=34))
 job_queue.run_daily(afternoon_reminder, time=time(hour=15, minute=0))
 job_queue.run_daily(night_reminder, time=time(hour=21, minute=0))
 
